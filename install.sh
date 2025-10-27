a2a940f7177d3fb04cf8396d3240214e75f0f02d
shalini-nik <shalininikhil22@gmail.com>
2025-10-27 22:42:00 +0530

    updated tmux and vimrc

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
+ln -sfn $(pwd)/vim ~/.vim
 curl -fLo  ~/.vim/autoload/plug.vim --create-dirs \
    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-ln -sfn $(pwd)/vim ~/.vim
 ln -sfn $(pwd)/tmux/tmux.conf ~/.tmux.conf
 echo "done."
